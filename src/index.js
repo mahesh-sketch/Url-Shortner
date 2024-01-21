ceef6518332959784dd11dd65fe528f0cb637bed
mahesh dash <maheshdash84821@gmail.com>
2024-01-21 23:15:48 +0530

    Updated main Index File

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import express from "express";
 import router from "../routes/index.js";
 import dbConnection from "../db/dbconnection.js";
-
+import staticRoutes from "../routes/staticRoutes.js";
+import path from "path"
 const app = express();
 
 //Database Connection
 dbConnection("mongodb://127.0.0.1:27017/shorturl");
 
+//Alert the express that ejs engine are used
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
 //Middleware Plugin
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use("/", staticRoutes);
 
 app.use("/shorturl", router);
 
