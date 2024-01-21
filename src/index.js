4bbb6e0867b12565e167db9ffec094ec07ed3fd1
mahesh dash <maheshdash84821@gmail.com>
2024-01-21 14:28:43 +0530

    Main Index File

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log("Mahesh");
+import express from "express";
+import router from "../routes/index.js";
+import dbConnection from "../db/dbconnection.js";
+
+const app = express();
+
+//Database Connection
+dbConnection("mongodb://127.0.0.1:27017/shorturl");
+
+//Middleware Plugin
+app.use(express.json());
+
+app.use("/shorturl", router);
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is listening at ${process.env.PORT}`);
+});
