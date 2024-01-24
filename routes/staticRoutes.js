c17b83ae34f2efac8d347022daaacf94dfe83a17
mahesh dash <maheshdash84821@gmail.com>
2024-01-24 12:19:24 +0530

    Auth and login router added

--- routes/staticRoutes.js
+++ routes/staticRoutes.js
@@ -4,10 +4,20 @@ import DbModel from "../model/url.model.js";
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-  const allUrls = await DbModel.find({});
+  if (!req.user) return res.redirect("/login");
+
+  const allUrls = await DbModel.find({ createdBy: req.user._id });
   return res.render("home", {
     urls: allUrls,
   });
+});
+
+router.get("/signup", async (req, res) => {
+  return res.render("signup");
+});
+
+router.get("/login", async (req, res) => {
+  return res.render("login");
 });
 
 export default router;
