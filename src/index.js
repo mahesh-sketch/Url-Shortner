4aa959742219d29dd7c26a496978fb5c221c650e
mahesh dash <maheshdash84821@gmail.com>
2024-01-24 12:17:49 +0530

    Updated Main indexfile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import router from "../routes/index.js";
 import dbConnection from "../db/dbconnection.js";
 import staticRoutes from "../routes/staticRoutes.js";
-import path from "path"
+import path from "path";
+import useRouter from "../routes/user.js";
+import cookieParser from "cookie-parser";
+import authMiddleware from "../middlewares/auth.js";
+
 const app = express();
 
 //Database Connection
@@ -15,9 +19,11 @@ app.set("views", path.resolve("./views"));
 //Middleware Plugin
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", staticRoutes);
+app.use(cookieParser());
 
-app.use("/shorturl", router);
+app.use("/", authMiddleware.checkOut, staticRoutes);
+app.use("/shorturl", authMiddleware.restrictToLoggedinUserOnly, router);
+app.use("/user", useRouter);
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is listening at ${process.env.PORT}`);
