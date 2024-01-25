092f34eff4db0a5817c9a8ae4fb974c4fff422c2
mahesh dash <maheshdash84821@gmail.com>
2024-01-25 12:51:06 +0530

    JWT added

--- services/auth.js
+++ services/auth.js
@@ -1,11 +1,24 @@
+// import jwt from "jsonwebtoken";
+// const secret = "Mahesh@123";
 const sessionIdToUserMap = new Map();
 
 function setUser(id, user) {
   sessionIdToUserMap.set(id, user);
+
+  //Addition of JWT TOKEN
+  // const payload = {
+  //   _id: user._id,
+  //   email: user.email,
+  // };
+  // return jwt.sign(payload, secret);
 }
 
 function getUser(id) {
   return sessionIdToUserMap.get(id);
+
+  //Verify The JWT token
+  if (!token) return null;
+  return jwt.verify(token, secret);
 }
 
 const authservice = {
