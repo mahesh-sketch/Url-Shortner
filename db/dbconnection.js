de40ad034d0541ad25f122f406913093bb3b822e
mahesh dash <maheshdash84821@gmail.com>
2024-02-27 00:35:31 +0530

    Updated

--- db/dbconnection.js
+++ db/dbconnection.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
-async function dbConnection(path) {
-  return await mongoose
-    .connect(path)
-    .then(() => {
-      console.log("Database connection is Successful");
-    })
-    .catch((err) => {
-      console.log("Error occurss", err);
-    });
-}
+const dbConnection = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/urlshortner`
+    );
+    console.log(
+      `\n MongoDB connected !! 
+      DB Host ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.log("MongoDb Error: ", error);
+    process.exit(1);
+  }
+};
 
 export default dbConnection;
--- src/index.js
+++ src/index.js
@@ -6,11 +6,24 @@ import path from "path";
 import useRouter from "../routes/user.js";
 import cookieParser from "cookie-parser";
 import authMiddleware from "../middlewares/auth.js";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config({
+  path: "./.env",
+});
+
 //Database Connection
-dbConnection("mongodb://127.0.0.1:27017/shorturl");
+dbConnection()
+  .then(() => {
+    app.listen(process.env.PORT || 8001, () => {
+      console.log(`Server is listening at ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO DB connecttin Failed !!", err);
+  });
 
 //Alert the express that ejs engine are used
 app.set("view engine", "ejs");
@@ -24,7 +37,3 @@ app.use(cookieParser());
 app.use("/", authMiddleware.checkOut, staticRoutes);
 app.use("/shorturl", authMiddleware.restrictToLoggedinUserOnly, router);
 app.use("/user", useRouter);
-
-app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server is listening at ${process.env.PORT}`);
-});
